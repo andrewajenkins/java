9833f160c8d954170b61b2c053a8975a220d441f
Andrew Jenkins <andrewajenkins@outlook.com>
2017-05-31 13:30:46 -0700

    add bubble sort comment

--- src/main/algo/sort/BubbleSort.java
+++ src/main/algo/sort/BubbleSort.java
@@ -1,5 +1,14 @@
 package main.algo.sort;
 
+/**
+ *  For each element in the array, n, bubble sort traverses the entire
+ *  array, n*n, giving it a worst case running time of O(n^2). For each
+ *  element it checks to see if the next element is smaller than the
+ *  the current element. If true it swaps them in place. Running over
+ *  the array n^2 times ensures that the array will be fully sorted
+ *  even in the case that it started completely reversed.
+ *
+ */
 public class BubbleSort
 {
 	public static <E extends Comparable<E>> E[] bubbleSort(E[] ga) {
