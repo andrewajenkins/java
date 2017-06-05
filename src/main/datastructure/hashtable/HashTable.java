c3093f70cf3ef80cb74ed6fd48e5ad4705ed72f5
Andrew Jenkins <andrewajenkins@outlook.com>
2017-06-05 14:20:22 -0700

    fix map constructor

--- src/main/datastructure/hashtable/HashTable.java
+++ src/main/datastructure/hashtable/HashTable.java
@@ -67,7 +67,6 @@ public class HashTable<K,V> implements Serializable, Cloneable, Map<K,V>{
         for(K key : keySet) {
             put(key, m.get(key));
         }
-        throw new UnsupportedOperationException();
     }
 
     /**
